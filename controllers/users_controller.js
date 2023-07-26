71ad38ecb9e92561e6751fd1a06e60a5dc74d57e
saicharan <saicharan_b200970ec@nitc.ac.in>
2023-07-27 01:32:08 +0530

    Some more changes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -16,7 +16,7 @@ module.exports.signUp = function (req, res) {
         res.redirect('/users/profile');
     }
     return res.render('user_sign_up', {
-        title: 'codeial | signUp'
+        title: 'PostPalace | signUp'
     })
 }
 
@@ -27,7 +27,7 @@ module.exports.signIn = function (req, res) {
         res.redirect('/users/profile');
     }
     return res.render('user_sign_in', {
-        title: 'codeial | signIn'
+        title: 'PostPalace | signIn'
     })
 }
 
